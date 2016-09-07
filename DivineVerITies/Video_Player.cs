c2dacad811f48c6d5da81e1177e34254cf9bd0cb
ttatotab <ttatotab@gmail.com>
2016-09-07 12:25:16 +0000

    albert u can pull now

--- DivineVerITies/Video_Player.cs
+++ DivineVerITies/Video_Player.cs
@@ -1,14 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 using Android.App;
-using Android.Content;
 using Android.OS;
-using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Plugin.Connectivity;
+using Plugin.Connectivity.Abstractions;
 
 namespace DivineVerITies
 {
@@ -21,18 +18,20 @@ namespace DivineVerITies
         private MediaController mediaController;
         private VideoView videoPlayer;
         private ProgressBar videoProgressBar;
+        private TextView statusMessageTextView;
         private void InitializeVideoPlayer()
         {
             videoPlayer = FindViewById<VideoView>(Resource.Id.PlayerVideoView);
             mediaController = new MediaController(this, true);
             videoProgressBar = FindViewById<ProgressBar>(Resource.Id.VideoProgressBar);
+            statusMessageTextView = FindViewById<TextView>(Resource.Id.StatusMessageTextView);
             videoPlayer.SetMediaController(mediaController);
 
         }
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-            SetContentView(Resource.Layout.video_player);
+            SetContentView(Resource.Layout.VidPlayer);
             startingPosition = (bundle != null) ? bundle.GetInt(VideoPositionKey):0;
             InitializeVideoPlayer();                     
             
@@ -40,27 +39,32 @@ namespace DivineVerITies
         protected override void OnStart()
         {
             base.OnStart();
+            CrossConnectivity.Current.ConnectivityChanged += OnCurrentConnectivityChanged;
             videoPlayer.Prepared += OnVideoPlayerPrepared;
             LaunchVideo();
         }
         protected override void OnStop()
         {
             base.OnStop();
+            CrossConnectivity.Current.ConnectivityChanged -= OnCurrentConnectivityChanged;
             videoPlayer.Prepared -= OnVideoPlayerPrepared;
         }
         private void LaunchVideo()
         {
-            string videoUri =selectedVideo.Link;
-            videoPlayer.SetVideoURI(Android.Net.Uri.Parse(videoUri));
-            videoPlayer.SeekTo(startingPosition);
-            videoPlayer.Start();
+            if(CrossConnectivity.Current.IsConnected && !videoPlayer.IsPlaying)
+            {
+                string videoUri = selectedVideo.Link;
+                videoPlayer.SetVideoURI(Android.Net.Uri.Parse(videoUri));
+                videoPlayer.SeekTo(startingPosition);
+                videoPlayer.Start();
+            }
+           
         }
         private void OnVideoPlayerPrepared(object sender, EventArgs e)
         {
-            videoProgressBar.Visibility = ViewStates.Gone;
+            UpdateVideoPlayerState(true);
             mediaController.SetAnchorView(videoPlayer);
             //show media controls for 3 seconds when video starts to play
-
             mediaController.Show(3000);
         }
 
@@ -88,9 +92,17 @@ namespace DivineVerITies
 
         private void UpdateVideoPlayerState(bool playVideo)
         {
-            if (!videoPlayer.IsPlaying)
+            if (!CrossConnectivity.Current.IsConnected)
             {
-                videoProgressBar.Visibility = (playVideo) ? ViewStates.Gone : ViewStates.Visible;
+                statusMessageTextView.Visibility = ViewStates.Visible;
+                videoProgressBar.Visibility = ViewStates.Gone;
+                videoPlayer.Visibility = ViewStates.Gone;
+            }
+            else if (!videoPlayer.IsPlaying)
+            {
+                videoProgressBar.Visibility = (playVideo) 
+                    ? ViewStates.Gone 
+                    : ViewStates.Visible;
                 videoPlayer.Visibility = ViewStates.Visible;
             }
 
@@ -101,7 +113,11 @@ namespace DivineVerITies
             base.OnConfigurationChanged(newConfig);
             UpdateVideoPlayerState(false);
         }
-
+        private void OnCurrentConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+            UpdateVideoPlayerState(false);
+            LaunchVideo();
+        }        
     }
 
 
