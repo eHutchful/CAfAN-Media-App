a95aea9dd424dbc62f4a92c8629d68eed3d1401d
akema-trebla <akematrebla@yahoo.com>
2017-05-12 14:36:43 +0000

    Feedback Done

--- DivineVerITies/Feedback.cs
+++ DivineVerITies/Feedback.cs
@@ -7,6 +7,9 @@ using Android.Support.V7.App;
 using SupportActionBar = Android.Support.V7.App.ActionBar;
 using SupportToolbar = Android.Support.V7.Widget.Toolbar;
 using Android.Support.Design.Widget;
+using Newtonsoft.Json.Linq;
+using DivineVerITies.Helpers;
+using Android.Content;
 
 namespace DivineVerITies
 {
@@ -18,6 +21,10 @@ namespace DivineVerITies
         private TextInputLayout Subject;
         private TextInputLayout Body;
         private FloatingActionButton fab;
+        private string username;
+        private NetworkStatusMonitor networkStatusMonitor;
+        ISharedPreferences pref;
+        ISharedPreferencesEditor edit;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -30,18 +37,71 @@ namespace DivineVerITies
             SupportActionBar.Title = "Have Your Say";
             SupportActionBar.SetDisplayHomeAsUpEnabled(true);
 
+            networkStatusMonitor = new NetworkStatusMonitor();
+
             Email = FindViewById<TextInputLayout>(Resource.Id.txtInputLayoutEmail);
             Subject = FindViewById<TextInputLayout>(Resource.Id.txtInputLayoutSubject);
             Body = FindViewById<TextInputLayout>(Resource.Id.txtInputLayoutBody);
             fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
 
-            fab.Click += (o, e) =>
+            pref = Application.Context.GetSharedPreferences("UserInfo", FileCreationMode.Private);
+            edit = pref.Edit();
+
+            username = pref.GetString("Username", string.Empty);
+
+            fab.Click += async (o, e) =>
             {
+                View anchor = o as View;
+
                 if (string.IsNullOrWhiteSpace(Email.EditText.Text)
                 || string.IsNullOrWhiteSpace(Subject.EditText.Text)
                 || string.IsNullOrWhiteSpace(Body.EditText.Text))
                 {
                     return;
+                }
+
+                JObject jo = new JObject();
+                jo.Add("email", Email.EditText.Text);
+                jo.Add("subject", Subject.EditText.Text);
+                jo.Add("body", Body.EditText.Text);
+                jo.Add("username", username);
+
+                try
+                {
+                    if (networkStatusMonitor.State == NetworkState.ConnectedData || networkStatusMonitor.State == NetworkState.ConnectedWifi)
+                    {
+                        var t = await AzureService.DefaultService.client.InvokeApiAsync("/api/feedback/sendsupportmail", jo);
+                        Snackbar.Make(anchor, "Thanks. We'll get back to you shortly.", Snackbar.LengthIndefinite).Show();
+                    }
+                    else if (networkStatusMonitor.State == NetworkState.Disconnected)
+                    {
+                        CreateAndShowDialog("Your device is currently not connected to the internet. Please checkyour data or WiFi connection and try again.", "Network Connectivity Error");
+                    }
+
+                }
+                catch (System.Exception ex)
+                {
+                    Snackbar.Make(anchor, "An error occured! Please try again.", Snackbar.LengthIndefinite)
+                    //.SetAction("RETRY", async delegate
+                    //{
+                    //    if (string.IsNullOrWhiteSpace(Email.EditText.Text)
+                    //    || string.IsNullOrWhiteSpace(Subject.EditText.Text)
+                    //    || string.IsNullOrWhiteSpace(Body.EditText.Text))
+                    //    {
+                    //        return;
+                    //    }
+
+                    //    JObject jo = new JObject();
+                    //    jo.Add("email", Email.EditText.Text);
+                    //    jo.Add("subject", Subject.EditText.Text);
+                    //    jo.Add("body", Body.EditText.Text);
+                    //    jo.Add("username", username);
+
+                    //    await AzureService.DefaultService.client.InvokeApiAsync("/feedback", jo);
+                    //    Snackbar.Make(anchor, "Thanks. We'll get back to you shortly.", Snackbar.LengthIndefinite).Show();
+                    //})
+                    .Show();
+
                 }
             };
         }
--- DivineVerITies/Helpers/AzureService.cs
+++ DivineVerITies/Helpers/AzureService.cs
@@ -1,16 +1,9 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 using Android.App;
 using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Microsoft.WindowsAzure.MobileServices;
-using ModernHttpClient;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.Net.Http;
